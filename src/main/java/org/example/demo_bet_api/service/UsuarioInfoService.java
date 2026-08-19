f50bcea385ec103f5168db190c7f0684615e7a93
nicollymazeika <nicollydepaula10@gmail.com>
2026-08-19 14:05:17 -0300

    feat: implementa service de usuarioInfo

---
+++ src/main/java/org/example/demo_bet_api/service/UsuarioInfoService.java
@@ -0,0 +1,36 @@
+package org.example.demo_bet_api.service;
+
+
+import org.example.demo_bet_api.model.Usuario;
+import org.example.demo_bet_api.model.UsuarioInfo;
+import org.example.demo_bet_api.repository.UsuarioInfoRepository;
+import org.example.demo_bet_api.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsuarioInfoService {
+    @Autowired
+    private UsuarioInfoRepository usuarioInfoRepository;
+
+    public UsuarioInfo findById(Integer id){
+        Optional<UsuarioInfo> usuarioInfo = usuarioInfoRepository.findById(id);
+        return usuarioInfo.orElse(null);
+    }
+
+    public List<UsuarioInfo> findAll(){
+        return usuarioInfoRepository.findAll();
+    }
+
+    public UsuarioInfo save(UsuarioInfo usuarioInfo){
+        return usuarioInfoRepository.save(usuarioInfo);
+    }
+
+    public void delete(Integer id){
+        usuarioInfoRepository.deleteById(id);
+    }
+}
+
